433fde0ca081678efab7f5e5516d0395f4d0a282
刘康明 <2478222603@qq.com>
2020-09-26 23:43:52 +0800

    hello_idea_git_dev-2

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -6,6 +6,6 @@ package com.atguigu;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git  idea");
+        System.out.println("hello git  idea  dev");
     }
 }
