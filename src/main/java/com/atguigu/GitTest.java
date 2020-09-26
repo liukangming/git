d53b48ede68f1859bad646447ae81b37f60b487e
刘康明 <2478222603@qq.com>
2020-09-26 23:34:53 +0800

    hello_idea_git

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author lkm
+ * @create 2020-09-26-23:29
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
